283b8651007c6b88656ed5fc822ea60922f20026
Ben Mondry <benmondrywork@gmail.com>
2023-12-31 18:55:33 +1100

    Added css to style the bars, bottom middle of the screen. Need to now implement the button to generate a new array.

--- sorting-visualizer/src/SortingVisualizer/SortingVisualizer.jsx
+++ sorting-visualizer/src/SortingVisualizer/SortingVisualizer.jsx
@@ -22,12 +22,14 @@ export default class SortingVisualizer extends React.Component{
         const {array} = this.state;
 
         return(
-            <>
+            <div className = "array-container">
                 {array.map((value, idx) => (
-                    <div className = "array-bar" key = {idx}> {value}
+                    <div className = "array-bar" 
+                    key = {idx}
+                    style = {{height: `${value}px`}}>
                     </div>
                 ))}
-            </>
+            </div>
         );
     }
 
