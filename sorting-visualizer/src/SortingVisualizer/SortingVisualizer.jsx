bac6f2226fdd88a69ecf3e34b88dee740b7ac921
Ben Mondry <benmondrywork@gmail.com>
2024-01-01 21:31:15 +1100

    Bubble sort algortithm implemented, and some animation have been applied, however there are still issues with them, and I need to spend time debugging.

--- sorting-visualizer/src/SortingVisualizer/SortingVisualizer.jsx
+++ sorting-visualizer/src/SortingVisualizer/SortingVisualizer.jsx
@@ -1,6 +1,15 @@
 import React from 'react';
+import {bubbleSort} from '../sortingAlgorithms/sortingAlgorithms.js';
 import './SortingVisualizer.css';
-import * as sortingAlgorithms from '../sortingAlgorithms/sortingAlgorithms.js';
+
+
+const ANIMATIONS_SPEED = 20;
+const NUMBER_OF_BARS = 120;
+const PRIMARY_COLOUR = 'turquoise';
+const SECONDARY_COLOUR = 'red';
+
+
+
 export default class SortingVisualizer extends React.Component{
     constructor(props){
         super(props);
@@ -13,21 +22,44 @@ export default class SortingVisualizer extends React.Component{
     }
     resetArray(){
         const array = [];
-        for(let i = 0;i < 100;i++){
+        for(let i = 0;i < NUMBER_OF_BARS;i++){
             array.push(randomIntFromInterval(5,730));
         }
         this.setState({array});
     }
     bubbleSort(){
+        
+       const animations = bubbleSort(this.state.array);
+       const arrayBars = document.getElementsByClassName('array-bar');
+       for(let i =0; i < animations.length; i++){
+        const isColourChange = i % 3 !==2;
+        if(isColourChange){
+            const [barOneIdx, barTwoIdx] = animations[i];
+            const barOneStyle = arrayBars[barOneIdx].style;
+            const barTwoStyle = arrayBars[barTwoIdx].style;
+            const colour = i % 3 === 0? SECONDARY_COLOUR : PRIMARY_COLOUR;
+            setTimeout(() => {
+                barOneStyle.backgroundColor = colour;
+                barTwoStyle.backgroundColor = colour;
+            }, i * ANIMATIONS_SPEED);
+        }else{
+            setTimeout(() => {
+                const scaling = 5
+                const [barOneIdx, newHeight] = animations[i];
+                const barOneStyle = arrayBars[barOneIdx].style;
+                barOneStyle.height = `${newHeight*scaling}px`;
+            }, i *ANIMATIONS_SPEED);
+        } 
+       }
+    }
+    testSort(){
         const javaScriptSortedArray = this.state.array
             .slice()
             .sort((a,b) => a-b);
-        const sortedArray = sortingAlgorithms.bubbleSort(this.state.array);
-        console.log(sortedArray);
-        console.log(arraysAreEqual(javaScriptSortedArray, sortedArray))
-        
+        const array = bubbleSort(this.state.array);
+        console.log(array);
+        console.log(arraysAreEqual(javaScriptSortedArray, array))
     }
-
     render(){
         const {array} = this.state;
 
@@ -58,4 +90,5 @@ function arraysAreEqual(arrayOne, arrayTwo){
             return false;
         }
     }
+    return true;
 }
--- sorting-visualizer/src/sortingAlgorithms/sortingAlgorithms.js
+++ sorting-visualizer/src/sortingAlgorithms/sortingAlgorithms.js
@@ -1,14 +1,24 @@
-export const bubbleSort = array =>{
-    const size = array.length
-    const sortedArray = array.slice()
-    for(let i =0; i<size;i++){
-        for(let j = 0; j<size;j++){
-            const temp = sortedArray[i]
-            if(sortedArray[i] > sortedArray[j]){
-                sortedArray[i] = sortedArray[j]
-                sortedArray[j] = temp
+export function bubbleSort(array){
+    const animations = []
+    let arr = array.slice(0);
+    const size = arr.length 
+    for(let i =0; i< size;i++){
+        for(let j = 0; j < size -i-1;j++){
+            //animations.push([j, j+1])
+            //animations.push([j, j+1])
+            if(arr[j] > arr[j+1]){
+                let temp = arr[j]
+                animations.push([j, j+1])
+                animations.push([j, j+1])
+                animations.push([j, j+1])
+                
+                //values we compare, so we push to change colour once.
+                // push again to revert colour 
+                arr[j] = arr[j+1]
+                arr[j+1] = temp
             }
         }
+        
     }
-    return sortedArray
+    return animations
 };
